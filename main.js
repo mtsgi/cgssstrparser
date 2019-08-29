6b028133b321400925ffd9ed6c66aa7cdd30561a
mtsgi <oq@live.jp>
2019-08-30 03:04:13 +0900

    Update UI

--- main.js
+++ main.js
@@ -2,16 +2,11 @@
 
 const reverse = () => {
     let bake = document.querySelector('#bake').value, orig = '';
-    for( let i of bake ) {
-        orig += String.fromCharCode( i.charCodeAt(0) - 288 )
-    }
+    for( let i of bake ) orig += String.fromCharCode( i.charCodeAt(0) - 288 );
     document.querySelector('#result').textContent = orig;
 }
-
 const parse = () => {
     let orig = document.querySelector('#orig').value, bake = '';
-    for( let i of orig ) {
-        bake += String.fromCharCode( i.charCodeAt(0) + 288 )
-    }
+    for( let i of orig ) bake += String.fromCharCode( i.charCodeAt(0) + 288 );
     document.querySelector('#result').textContent = bake;
 }
